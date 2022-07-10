594f286fda11c328ad95b715e657947299d8d084
@codingschool_tdomanig <tobias.domanig@edu.fh-kaernten.ac.at>
2022-07-11 00:57:11 +0200

    added routing to rocketdetails

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Layout } from "./common/components/layout";
 import { RocketsDetailPage } from "./features/rocket-detail/rocket-detail";
+
 export const AppRouter = () => {
   return (
-    <>
-      <Layout />
-      <RocketsDetailPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Layout />
+              <RocketsDetailPage />
+            </>
+          }
+        >
+          <Route
+            path="rocketdetail/:rocketDettailId"
+            element={<RocketsDetailPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
